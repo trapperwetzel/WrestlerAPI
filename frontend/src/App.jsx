671012bf0ec388390cde0f3d2c1351c4d28b8c86
tcoding <trapperwetzel1@gmail.com>
2025-06-28 23:17:58 -0500

    css updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,26 @@ import './App.css'
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import styled from '@emotion/styled';
 import { WrestlerTable } from './components/WrestlerTable.jsx';
+
+
+
 const Title = styled.h1`
-    text-align: center;
-    font-color: white;
+  text-align: center;
+  font-size: 2.5rem;
+  font-weight: 700;
+  color:rgb(228, 224, 224);
+  margin-bottom: 2rem;
 `;
 
 
 function App() {
     return (
-    <div style={{ maxWidth: 1000, margin: 'auto', paddingTop: '2rem' }}>
+    
+    <div style={{  margin: 'auto', paddingTop: '2rem' }}>
         <Title>Pro Wrestling Champions</Title>
         <WrestlerTable />
     </div>
+    
 
     );
 }
