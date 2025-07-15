90a73ce4b92c95dc7ae0f2ea9e2dc30ed6e123ad
tcoding <trapperwetzel1@gmail.com>
2025-07-14 19:58:23 -0500

    fetchUS Champs

--- backend/services/fetchUSChampions.js
+++ backend/services/fetchUSChampions.js
@@ -1,11 +1,11 @@
 import fetch from 'node-fetch';
 import { JSDOM } from 'jsdom';
 
-async function fetchICChampions() {
-  console.log("Starting fetch for IC Champions");
+async function fetchUSChampions() {
+  console.log("Starting fetch for US Champions");
 
   try {
-    const resp = await fetch("https://en.wikipedia.org/w/api.php?action=parse&page=List_of_WWE_Intercontinental_Champions&format=json");
+    const resp = await fetch("https://en.wikipedia.org/w/api.php?action=parse&page=List_of_WWE_United_States_Champions&format=json");
     const data = await resp.json();
 
     const htmlContent = data?.parse?.text?.["*"];
@@ -30,10 +30,11 @@ async function fetchICChampions() {
         cell.textContent.trim().replace(/\[\d+\]/g, '').replace(/\s+/g, ' ')
       );
 
-      if (values.length < 4) return;
+      
 
       const firstCell = values[0]?.trim();
       let nameIndex = isNaN(firstCell) ? 0 : 1;
+      console.log("Name Index:", nameIndex, "Name:", values[nameIndex]);
       const name = values[nameIndex]?.replace(/["']/g, '').trim();
 
       const reignsIndex = nameIndex + 1;
@@ -62,7 +63,7 @@ async function fetchICChampions() {
         transformedData.push({
           name: name,
           championship: {
-            championshipName: "IC Championship",
+            championshipName: "US Championship",
             totalReigns,
             totalDaysHeld,
           },
@@ -71,7 +72,7 @@ async function fetchICChampions() {
     });
 
     transformedData.sort((a, b) => b.totalDaysHeld - a.totalDaysHeld);
-
+    console.table(transformedData);
     return transformedData;
 
   } catch (err) {
@@ -80,4 +81,4 @@ async function fetchICChampions() {
   }
 }
 
-export default fetchICChampions;
+export default fetchUSChampions;
